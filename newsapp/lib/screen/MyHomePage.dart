88d2f18d62d3ea84281126887d69d566aedaa14a
LopsangLama00 <llopsang123@gmail.com>
2023-03-29 17:55:52 +0545

    final commit

---
+++ newsapp/lib/screen/MyHomePage.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:newsapp/model/News.dart';
+import 'package:flutter/material.dart';
+import 'package:newsapp/screen/newsDetails.dart';
+import 'package:newsapp/widgets/newsCard.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<News> _news = [];
+  @override
+  initState() {
+    onGetNews();
+  }
+
+  onGetNews() async {
+    try {
+      var url = Uri.https('api.jsonserve.com', '/fqpN3J');
+      var response = await http.get(url);
+      print('Response status: ${response.statusCode}');
+      jsonDecode(response.body)['articles']
+          .forEach((news) => {_news.add(News.fromJson(news))});
+      setState(() {});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News'),
+      ),
+      body: ListView(
+        children: [
+          ..._news.map(
+            (e) => InkWell(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => NewsDetails(
+                            news: e,
+                          )));
+                },
+                child: NewsCard(
+                  news: e,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ newsapp/lib/widgets/newsCard.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/model/News.dart';
+
+class NewsCard extends StatelessWidget {
+  News news;
+  NewsCard({required this.news});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    news.title,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+                  ),
+                  Text(
+                    news.content,
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    "GautamNews.com",
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.grey),
+                  ),
+                ],
+              )),
+          Expanded(flex: 1, child: Image.network(news.urlToImage)),
+        ],
+      ),
+    );
+  }
+}
